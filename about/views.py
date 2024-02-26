c9b97685e922d81c4f229d8cc48c67d2978b5def
Saki Chy <sakibchowdhury79@gmail.com>
2024-02-26 07:47:17 +0000

    add a form in about

--- about/views.py
+++ about/views.py
@@ -1,16 +1,29 @@
 from django.shortcuts import render
 from .models import About
+from .forms import CollaborateForm
+from django.contrib import messages
+
 
 # Create your views here.
 
 def about_me(request):
+    if request.method == "POST":
+        collaborate_form = CollaborateForm(data=request.POST)
+        if collaborate_form.is_valid():
+            collaborate_form.save()
+            messages.add_message(request, messages.SUCCESS, 
+            "Collaboration request received! I endeavour to respond within 2 working days.")
     """
     Renders the About page
     """
     about = About.objects.all().order_by('-updated_on').first()
+    collaborate_form = CollaborateForm()
 
     return render(
         request,
         "about/about.html",
-        {"about": about},
+        {"about": about, 
+        "collaborate_form": collaborate_form,
+        },
+        
     )
--- blog/views.py
+++ blog/views.py
@@ -34,6 +34,7 @@ def post_detail(request, slug):
     comment_count = post.comments.filter(approved=True).count()
 
     if request.method == "POST":
+       print("Received a POST")
        comment_form = CommentForm(data=request.POST)
        if comment_form.is_valid():
         comment = comment_form.save(commit=False)
@@ -46,7 +47,8 @@ def post_detail(request, slug):
     )
 
     comment_form = CommentForm()
-
+    print("About to render template...")
+    
     return render(
         request,
         "blog/post_detail.html",
